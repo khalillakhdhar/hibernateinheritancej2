f49eae054172af7df147c4a4a77ffb1fa44c5db2
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-06-29 10:30:08 +0100

    Initial commit

---
+++ src/main/java/com/example/demo/Hibernateinheritancej2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hibernateinheritancej2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hibernateinheritancej2Application.class, args);
+	}
+
+}
