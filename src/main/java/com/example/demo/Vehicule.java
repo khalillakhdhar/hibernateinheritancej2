5c32f2856d84e2deaacbdd81216ffdaac6264cdf
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-06-29 10:39:58 +0100

    Update Vehicule.java

--- src/main/java/com/example/demo/Vehicule.java
+++ src/main/java/com/example/demo/Vehicule.java
@@ -1,5 +1,28 @@
 package com.example.demo;
 
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.Entity;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+@DiscriminatorColumn(name = "type")
 public class Vehicule {
+private long idVehicule;
+private String manifacturer;
+public long getIdVehicule() {
+	return idVehicule;
+}
+public void setIdVehicule(long idVehicule) {
+	this.idVehicule = idVehicule;
+}
+public String getManifacturer() {
+	return manifacturer;
+}
+public void setManifacturer(String manifacturer) {
+	this.manifacturer = manifacturer;
+}
+
 
 }
